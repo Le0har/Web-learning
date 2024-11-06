aca0b1666c66da75a61ee3ca9003631d14d7aa28
Aleksey Tikhonov <leohar@mail.ru>
2024-11-06 15:00:37 +0300

    use Decimal

--- Unittest/QuadraticEquation/quadratic_equation.py
+++ Unittest/QuadraticEquation/quadratic_equation.py
@@ -1,20 +1,25 @@
-import math
+from decimal import Decimal, getcontext
 
 
 def solve_quadratic_equation(a, b, c):      # ax^2 + bx + c = 0
-    if math.isclose(a, 0):
+    epsilon = 6
+    getcontext().prec = epsilon
+    a = Decimal(a)
+    b = Decimal(b)
+    c = Decimal(c)
+    if a.quantize(Decimal('1.00000')) == 0:
         raise ValueError('The coefficient "a" cannot be equal to zero')
     roots_equation = []
     discriminant = b ** 2 - 4 * a * c
     if discriminant < 0:
         return roots_equation
-    elif math.isclose(discriminant, 0, abs_tol=0.00001):
+    elif discriminant == 0:
         x = -b / (2 * a)
         roots_equation.append(x)
         return roots_equation
     elif discriminant > 0:
-        x1 = (-b + discriminant ** 0.5) / (2 * a)
-        x2 = (-b - discriminant ** 0.5) / (2 * a) 
+        x1 = (-b + discriminant.sqrt()) / (2 * a)
+        x2 = (-b - discriminant.sqrt()) / (2 * a) 
         roots_equation.extend([x1, x2])
         return roots_equation
 
--- Unittest/QuadraticEquation/test_quadratic_equation.py
+++ Unittest/QuadraticEquation/test_quadratic_equation.py
@@ -1,29 +1,33 @@
 import unittest
-import quadratic_equation
+from quadratic_equation import solve_quadratic_equation
+from decimal import Decimal
 
 
 class TestQadraticEquation(unittest.TestCase):
     def test_no_roots(self):
-        self.assertEqual(quadratic_equation.solve_quadratic_equation(1, 0, 1), [])               # task test 1
-        self.assertEqual(quadratic_equation.solve_quadratic_equation(1, -5, 9), [])
-        self.assertEqual(quadratic_equation.solve_quadratic_equation(4, 0, 8), [])
+        self.assertEqual(solve_quadratic_equation(1, 0, 1), [])                 # task test 1
+        self.assertEqual(solve_quadratic_equation(1, -5, 9), [])
+        self.assertEqual(solve_quadratic_equation(4, 0, 8), [])
 
     def test_one_root(self):
-        self.assertEqual(quadratic_equation.solve_quadratic_equation(1, 2, 1), [-1])            # task test 3
-        self.assertEqual(quadratic_equation.solve_quadratic_equation(1, 2, 0.99999975), [-1])   # task test 5
-        self.assertEqual(quadratic_equation.solve_quadratic_equation(1, -12, 36), [6])
-        self.assertEqual(quadratic_equation.solve_quadratic_equation(4, -12, 9), [1.5])
+        self.assertEqual(solve_quadratic_equation(1, 2, 1), [-1])               # task test 3
+        self.assertEqual(solve_quadratic_equation(1, 2, 0.99999975), [-1])      # task test 5
+        self.assertEqual(solve_quadratic_equation(1, -12, 36), [6])
+        self.assertEqual(solve_quadratic_equation(4, -12, 9), [1.5])
+        self.assertEqual(solve_quadratic_equation(1, 1, 0.2500000003), [-0.5])  # D = -0.0000000012
 
     def test_two_roots(self):
-        self.assertEqual(quadratic_equation.solve_quadratic_equation(1, 0, -1), [1, -1])        # task test 2
-        self.assertEqual(quadratic_equation.solve_quadratic_equation(3, 11, 6), [-0.6666666666666666, -3])
-        self.assertEqual(quadratic_equation.solve_quadratic_equation(1, 17, -18), [1, -18])  
-        self.assertEqual(quadratic_equation.solve_quadratic_equation(-3, 0, 75), [-5, 5])
-        self.assertEqual(quadratic_equation.solve_quadratic_equation(2, 4, 0), [0, -2])  
+        self.assertEqual(solve_quadratic_equation(1, 0, -1), [1, -1])           # task test 2
+        self.assertEqual(solve_quadratic_equation(3, 11, 6), [Decimal('-0.666667'), -3])
+        self.assertEqual(solve_quadratic_equation(1, 17, -18), [1, -18])  
+        self.assertEqual(solve_quadratic_equation(-3, 0, 75), [-5, 5])
+        self.assertEqual(solve_quadratic_equation(2, 4, 0), [0, -2])  
 
-    def coefficient_a_equal_zero(self):
+    def test_coefficient_a_equal_zero(self):
         with self.assertRaises(ValueError):
-            quadratic_equation.solve_quadratic_equation(0, 4, 5)                                # task test 4
+            solve_quadratic_equation(0, 4, 5)                                   # task test 4
+        with self.assertRaises(ValueError):
+            solve_quadratic_equation(-0.0000000012, 4, 5)                       # a = -0.0000000012           
 
 
 if __name__ == '__main__':
